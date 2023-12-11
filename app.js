c6e2c88a2d18381c414b4093f318ef1297d0b3ad
noaysk <nyasukagawa@myseneca.ca>
2023-12-10 23:57:19 -0500

    connected sql

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
+
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "progate",
+//   password: "password",
+//   database: "list_app",
+// });
+
+require("dotenv").config();
+
+const mysql = require('mysql2');
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user:process.env.DB_USER,
+    database:process.env.DB_NAME,
+    password:process.env.DB_PASSWORD,
+})
+
 
 // top
-app.get('/', (req, res) => {
-  res.render('top.ejs');
+app.get("/", (req, res) => {
+  res.render("top.ejs");
 });
 
 // list view
-app.get('/index', (req, res) => {
-  res.render('index.ejs');
+app.get("/index", (req, res) => {
+  pool.query("SELECT * FROM items", (error, results) => {
+    res.render("index.ejs", { items: results });
+  });
 });
-
 // Code to start the server
 app.listen(3000);
+
+module,exports = pool.promise();
+
