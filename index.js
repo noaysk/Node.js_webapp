d38bc4a8ecc467e7794172f6ef3e495fdbdbfac5
noaysk <nyasukagawa@myseneca.ca>
2023-12-16 19:30:02 -0500

    changed name app.js to index.js

--- index.js
+++ index.js
@@ -1,32 +1,57 @@
-// referenced by https://github.com/expressjs/express/blob/e1b45ebd050b6f06aa38cda5aaf0c21708b0c71e/lib/application.js
-// and https://github.com/mysqljs/mysql/blob/master/lib/Connection.js
+const express = require("express");
+const app = express();
 
-const app = require('express').application;
-const mysql = require('mysql');
-const http = require('http');
-const res = require('express').response;
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+require("dotenv").config();
 
-mysql.originalCreateConnection = mysql.createConnection;
-res.originalRedirect = res.redirect;
+const mysql = require("mysql2");
 
-// Override app.listen
-app.listen = function listen(port) {
-  var server = http.createServer(this);
-  return server.listen.apply(server, [3000]);
-};
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+});
 
-// Override createConnection
-mysql.createConnection = function createConnection(config) {
-  var _this = this;
-  if (process.env.IDENTIFIER === 'ANSWER'){
-    config.database = 'answer_' + config.database;
-  }
-  return mysql.originalCreateConnection.call(_this, config);
-}
+// require("dotenv").config();
 
-// Override res.redirect
-res.redirect = function redirect(url) {
-  var _this = this;
-  new_url = url + "?containerPort=3000&languageName=nodejs";
-  return res.originalRedirect.call(_this, new_url);
-};
+// const mysql = require("mysql2");
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+// });
+
+// top
+app.get("/", (req, res) => {
+  res.render("top.ejs");
+});
+
+// list view
+app.get("/index", (req, res) => {
+  connection.query("SELECT * FROM items", (error, results) => {
+    res.render("index.ejs", { items: results });
+  });
+});
+
+// create
+app.get("/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+app.post("/create", (req, res) => {
+  connection.query(
+    "insert into items (number, name) values(?, ?)",
+    [req.body.itemNum, req.body.itemName],
+    (error, results) => {
+      res.redirect("/index");
+    }
+  );
+});
+
+// Code to start the server
+app.listen(3000);
+
+// module, (exports = pool.promise());
